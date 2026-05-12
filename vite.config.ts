ef268dc10b0c20df74e0fdb1f08c79fbaf22e074
yi890 <jicr109@gmail.com>
2026-05-11 23:29:39 -0600

    Fix PWA manifest and title after rename

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,9 @@ export default defineConfig(({mode}) => {
               purpose: 'any maskable'
             }
           ]
+        },
+        devOptions: {
+          enabled: true
         }
       })
     ],
